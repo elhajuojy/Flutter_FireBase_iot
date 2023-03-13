b9804aa5fee36e449e74d8b8767d463bb0cc913c
elhajuojy <souirimehdi311@gmail.com>
2023-03-13 21:47:23 +0100

    Ui for the Home Page mostly done

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,21 +15,149 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat.yMMMEd().format(now);
+    var formattedTime = DateFormat.jm().format(now);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              logout();
-            },
-            icon: const Icon(Icons.logout),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text("welcome to Home Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Flutter Firebase IoT"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                logout();
+              },
+              icon: const Icon(Icons.logout),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text("Tableau de bord ",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              Card(
+                  child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                    width: Get.width,
+                    child: Text("Date : $formattedDate $formattedTime",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ))),
+              )),
+              Card(
+                  child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                    width: Get.width,
+                    child: const Text("Plante Actuelle : Mint",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ))),
+              )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                          width: Get.width / 3,
+                          height: 100,
+                          child: const Center(
+                            child: Text("Température : 25°C",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal,
+                                )),
+                          )),
+                    )),
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                          width: Get.width / 3,
+                          height: 100,
+                          child: const Center(
+                            child: Text("Humidité : 50%",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal,
+                                )),
+                          )),
+                    )),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                          width: Get.width / 3,
+                          height: 100,
+                          child: const Center(
+                            child: Text("Water Level : 50%",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal,
+                                )),
+                          )),
+                    )),
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                          width: Get.width / 3,
+                          height: 100,
+                          child: const Center(
+                            child: Text("Humidité : 50%",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal,
+                                )),
+                          )),
+                    )),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: Get.width,
+                child: InkWell(
+                  onTap: () {
+                    //todo : refersh data from firebase
+                  },
+                  child: const Text("Refresh",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+              )
+            ],
+          )),
+        ));
   }
 }
